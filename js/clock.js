464effdf7a03b22d2ce50c4c5e2da29c0d981527
raymag <prayk11@outlook.com>
2021-02-26 21:27:31 -0300

    feat: let there be light

---
+++ js/clock.js
@@ -0,0 +1,26 @@
+var interval;
+const maxHeight = 30;
+const timespan = .5 * 60; // 30s
+var currentTime = timespan;
+const emptyness = document.querySelector("#emptyness");
+const intervalTime = 2.5; 
+
+function startTimer(){
+    console.log("Timer Started");
+    interval = setInterval(fillClock, intervalTime * 1000);
+}
+
+function fillClock(){
+    console.log("Filling Clock");
+    currentTime -= intervalTime;
+    const emptynessHeightPercentage = (100 * currentTime)/timespan;
+    const emptynessHeight = maxHeight * (emptynessHeightPercentage/100);
+    emptyness.style.height = `${emptynessHeight}em`;
+    
+    if (emptynessHeight <= 0) {
+        console.log("Time Stoped");
+        clearInterval(interval);
+    }
+}
+
+startTimer();
